74f2fa0fb671a3b8b4021bc7e7e3d4b8e839c890
LeonTanemura <leontanemura@gmail.com>
2024-05-15 16:56:32 +0900

    fix: 最高精度

--- experiment/optuna.py
+++ experiment/optuna.py
@@ -42,16 +42,10 @@ def lightgbm_config(trial: optuna.Trial, model_config, name=""):
     return model_config
 
 def catboost_config(trial: optuna.Trial, model_config, name=""):
-    # model_config.iterations = trial.suggest_int("iterations", 500, 5000)
     model_config.depth = trial.suggest_int("depth", 3, 10)
     model_config.learning_rate = trial.suggest_float("learning_rate", 1e-5, 1.0)
     model_config.random_strength = trial.suggest_int("random_strength", 0, 100)
     model_config.bagging_temperature = trial.suggest_float("bagging_temperature", 0.01, 100.00, log=True)
-    # model_config.od_type = trial.suggest_categorical("od_type", ["IncToDec", "Iter"])
-    # model_config.od_wait = trial.suggest_int("od_wait", 10, 50)
-    # model_config.subsample = trial.suggest_float("subsample", 0.05, 1.0)
-    # model_config.colsample_bylevel = trial.suggest_float("colsample_bylevel", 0.05, 1.0)
-    # model_config.min_data_in_leaf = trial.suggest_int("min_data_in_leaf", 1, 100)
     return model_config
 
 
